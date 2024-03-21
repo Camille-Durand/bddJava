dd10ccacf7a35a5bdef2f9936b7a245fe001fa1e
Camille-Durand <camillefanguin@gmail.com>
2024-03-21 14:05:55 +0100

    Update main.java

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,18 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Utilisateur user = new Utilisateur("newtest", "newtest", "test@new.com", "1234");
+        Utilisateur user = new Utilisateur("Test", "Testing", "Test@ing.com", "1234");
 
-        /*
-
-        //test si le compte exist
+        // Vérification compte existant
         if(Objects.equals(user.getEmail(), ManagerUtilisateur.findByMail(user).getEmail())){
             System.out.println("Le compte existe déja");
         }
         else{
-            //ajouter le compte en BDD
+            // Création nouveau compte
             System.out.println(ManagerUtilisateur.create(user));
         }
 
-         */
-
+        // Update compte existant
         ManagerUtilisateur.update(user);
     }
 }
