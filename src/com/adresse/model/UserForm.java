869089bf7fb4ea5efd6d1ec4eec7a71b6add58a6
Camille-Durand <camillefanguin@gmail.com>
2024-03-22 09:55:26 +0100

    Modification UserForm + ajout ActionListener btn Ajouter

--- src/com/adresse/model/UserForm.java
+++ src/com/adresse/model/UserForm.java
@@ -2,6 +2,8 @@ package com.adresse.model;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class UserForm extends JDialog{
     private JLabel jlName; // ok
@@ -15,6 +17,8 @@ public class UserForm extends JDialog{
     private JButton btValid; // ok
     private JButton btCancel; // ok
     private JPanel jpMain; // ok
+    private JLabel jlVerify; // ok
+    private JPasswordField pfVerify; // ok
 
     public UserForm(JDialog parent){
         super(parent);
@@ -22,6 +26,32 @@ public class UserForm extends JDialog{
         setContentPane(jpMain);
         setMaximumSize(new Dimension(800,600));
         setMinimumSize(new Dimension(800,600));
-        setVisible(true);
+        setVisible(true);        //écouteur événement bt valider
+        btValid.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = tfName.getText();
+                String firstname = tfFirstname.getText();
+                String email = tfEmail.getText();
+                String password = String.valueOf(pfPassword.getPassword());
+                String verify = String.valueOf(pfVerify.getPassword());
+
+                if(!name.isBlank() && !firstname.isBlank() && !email.isBlank() && !password.isBlank()){
+                    if(password.equals(verify)){
+                        Utilisateur user = new Utilisateur(name,firstname,email,password);
+                        System.out.println("Utilisateur ajouté dans la BDD avec succès :)");
+                        System.out.println(user);
+                    } else {
+                        System.out.println("Les 2 mots de passe de correspondent pas");
+                    }
+                }
+            }
+        });
+        btCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
     }
 }
